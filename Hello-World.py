178d721ba02ba3339b728f2f219e7e97a7528a2c
Ubuntu <ubuntu@ip-172-31-19-198.ec2.internal>
2019-09-12 18:41:18 +0000

    initial commit

---
+++ Hello-World.py
@@ -0,0 +1,13 @@
+# 1/usr/bin/env python3
+
+# Created by: Douglass Jeffrey
+# Created on: September 2019
+# This is the "Hello, World!" program with a fancy style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
